d029284d532c1ba22688a4fbdc0e956575c868d9
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-04-09 21:42:31 +0530

    all done

--- Web monitor/app.py
+++ Web monitor/app.py
@@ -19,13 +19,6 @@ def index():
     pd.set_option('display.max_columns', None)
     print(dataframe.head())
     
-
-
-
-
-
-
-
     """Views vs Days ago"""
     fig1 = px.scatter(dataframe, 
                  x='days ago', 
@@ -57,13 +50,6 @@ def index():
     yaxis=dict(tickformat=",")
     )
     print(fig1)
-
-
-
-
-
-
-
 
     """Likes vs Days ago"""
     fig2 = px.scatter(dataframe, 
@@ -97,11 +83,6 @@ def index():
     )
     print(fig1)
 
-
-
-
-
-
     """Views vs likes"""
     fig3 = px.scatter(dataframe,
                     x='views',
@@ -121,13 +102,6 @@ def index():
         '<b>Products</b>: %{customdata[1]}<br>',
         customdata=dataframe[['video title', 'products']]
     )
-
-
-
-
-
-
-
 
     """Likes per View vs Days Ago"""
 
@@ -150,13 +124,6 @@ def index():
         '<b>Products</b>: %{customdata[1]}<br>',
         customdata=dataframe[['video title', 'products']]
     )
-
-
-
-
-
-
-
 
     """Views per Day (Growth Rate) vs Days Ago"""
     dataframe['views_per_day'] = dataframe['views'] / dataframe['days ago']
@@ -195,6 +162,63 @@ def index():
                         graphJSON3=graphJSON3,
                         graphJSON4=graphJSON4,
                         graphJSON5=graphJSON5)
+
+@app.route('/get_charts')
+def get_charts():
+    dataframe = pd.read_json(r"D:\YouTube-Likes-predictor\Web monitor\kzg.json")
+    dataframe = pd.json_normalize(dataframe['videos'])
+    dataframe["likes"] = dataframe['likes'].apply(convert_likes)
+    dataframe["views"] = dataframe['views'].str.replace(',', '').astype(int)
+
+    dataframe['likes_per_view'] = dataframe['likes'] / dataframe['views']
+    dataframe['views_per_day'] = dataframe['views'] / dataframe['days ago']
+
+    # Chart 1: Views vs Days Ago
+    fig1 = px.scatter(dataframe, x='days ago', y='views', size='likes', color='products',
+                      title='Views vs Days Ago',
+                      labels={'days ago': 'Days Since Upload', 'views': 'View Count'})
+    fig1.update_traces(customdata=dataframe[['video title', 'products']],
+                       marker=dict(line=dict(width=1, color='DarkSlateGrey')))
+    
+    # Chart 2: Likes vs Days Ago
+    fig2 = px.scatter(dataframe, x='days ago', y='likes', size='views', color='products',
+                      title='Likes vs Days Ago',
+                      labels={'days ago': 'Days Since Upload', 'likes': 'Likes Count'})
+    fig2.update_traces(customdata=dataframe[['video title', 'products']],
+                       marker=dict(line=dict(width=1, color='DarkSlateGrey')))
+
+    # Chart 3: Likes vs Views
+    fig3 = px.scatter(dataframe, x='views', y='likes', size='days ago', color='products',
+                      title='Likes vs Views',
+                      labels={'views': 'View Count', 'likes': 'Likes Count'})
+    fig3.update_traces(customdata=dataframe[['video title', 'products']],
+                       marker=dict(line=dict(width=1, color='DarkSlateGrey')))
+
+    # Chart 4: Likes/View vs Days Ago
+    fig4 = px.scatter(dataframe, x='days ago', y='likes_per_view', size='views', color='products',
+                      title='Likes per View vs Days Ago',
+                      labels={'days ago': 'Days Since Upload', 'likes_per_view': 'Likes/View'})
+    fig4.update_traces(customdata=dataframe[['video title', 'products']],
+                       marker=dict(line=dict(width=1, color='DarkSlateGrey')))
+
+    # Chart 5: Views/Day vs Days Ago
+    fig5 = px.scatter(dataframe, x='days ago', y='views_per_day', size='likes', color='products',
+                      title='Views per Day vs Days Ago',
+                      labels={'days ago': 'Days Since Upload', 'views_per_day': 'Views/Day'})
+    fig5.update_traces(customdata=dataframe[['video title', 'products']],
+                       marker=dict(line=dict(width=1, color='DarkSlateGrey')))
+
+    return {
+        'chart1': json.loads(json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)),
+        'chart2': json.loads(json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)),
+        'chart3': json.loads(json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)),
+        'chart4': json.loads(json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)),
+        'chart5': json.loads(json.dumps(fig5, cls=plotly.utils.PlotlyJSONEncoder)),
+    }
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
